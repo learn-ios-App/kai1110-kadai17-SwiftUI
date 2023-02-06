25e51ad058ce596e931abab01abf57c1f765acaf
akio0911 <akio0911@gmail.com>
2023-02-06 12:40:39 +0900

    feedback

--- kai1110-kadai17-SwiftUI/ContentView.swift
+++ kai1110-kadai17-SwiftUI/ContentView.swift
@@ -2,7 +2,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject private var fruitData = FruitData()
+    @StateObject private var fruitData = FruitData()
     @State private var isAddView = false
     var body: some View {
         NavigationStack {
